851bc78c7d3c6d97c62c9d3cc68f8c6cfe900749
chalish <git@chalish.dev>
2026-06-13 00:10:15 +0300

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cubric - A cubing benchmark for LLMs
+
+Will finish this by adding things like OLL, F2L, and random 1-move / 2-move cases.
+
+But can't fully test it bc I'm broke lol. Even cheap models like Gemini Flash cost like 5$ for a single run (low reasoning, 21 cases, and only 1 trial for each case), the costs of Opus (and now Fable) and GPT 5.5 on high reasoning levels would be interesting.
--- packages/cube/src/algorithms.ts
+++ packages/cube/src/algorithms.ts
@@ -2,9 +2,9 @@ export type Algorithm = { name: string; algorithm: string };
 
 export const pllAlgorithms: Algorithm[] = [
   // TODO: Make sure all of these have correct AUF at the end. It's important for the base case
-  { name: "Aa", algorithm: "x R' U R' D2 R U' R' D2 R2 x'" },
-  { name: "Ab", algorithm: "x R2 D2 R U R' D2 R U' R x'" },
-  { name: "F", algorithm: "R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R" },
+  { name: "Aa", algorithm: "x R' U R' D2 R U' R' D2 R2 x'" }, // correct
+  { name: "Ab", algorithm: "x R2 D2 R U R' D2 R U' R x'" }, // correct
+  { name: "F", algorithm: "R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R" }, // correct
   { name: "Ga", algorithm: "R2 U R' U R' U' R U' R2 U' D R' U R D'" },
   { name: "Gb", algorithm: "R' U' R U D' R2 U R' U R U' R U' R2 D" },
   { name: "Gc", algorithm: "R2 U' R U' R U R' U R2 U D' R U' R' D" },
@@ -13,17 +13,17 @@ export const pllAlgorithms: Algorithm[] = [
   { name: "Jb", algorithm: "R U R' F' R U R' U' R' F R2 U' R' U'" },
   { name: "Ra", algorithm: "R U' R' U' R U R D R' U' R D' R' U2 R'" },
   { name: "Rb", algorithm: "R2 F R U R U' R' F' R U2 R' U2 R" },
-  { name: "T", algorithm: "R U R' U' R' F R2 U' R' U' R U R' F'" },
-  { name: "E", algorithm: "x' L' U L D' L' U' L D L' U' L D' L' U L D x" },
+  { name: "T", algorithm: "R U R' U' R' F R2 U' R' U' R U R' F'" }, // correct
+  { name: "E", algorithm: "x' R U' R' D R U R' D' R U R' D R U' R' D' x" }, // correct
   {
     name: "Na",
-    algorithm: "R U R' U R U R' F' R U R' U' R' F R2 U' R' U2 R U' R'",
+    algorithm: "R U R' U R U R' F' R U R' U' R' F R2 U' R' U2 R U' R'", // correct, U2 might give us a more intuitive view but might break the AUF
   },
-  { name: "Nb", algorithm: "R' U R U' R' F' U' F R U R' F R' F' R U' R" },
-  { name: "V", algorithm: "R' U R' U' y R' F' R2 U' R' U R' F R F y'" },
-  { name: "Y", algorithm: "F R U' R' U' R U R' F' R U R' U' R' F R F'" },
-  { name: "H", algorithm: "M2 U M2 U2 M2 U M2" },
-  { name: "Ua", algorithm: "R U' R U R U R U' R' U' R2" },
-  { name: "Ub", algorithm: "R2 U R U R' U' R' U' R' U R'" },
-  { name: "Z", algorithm: "M' U M2 U M2 U M' U2 M2" },
+  { name: "Nb", algorithm: "R' U R U' R' F' U' F R U R' F R' F' R U' R" }, // correct, U2 might give us a more intuitive view but then it'd break the AUF
+  { name: "V", algorithm: "R' U R' U' y R' F' R2 U' R' U R' F R F y'" }, // correct, that y' at the end is necessary to get the intuitive base view
+  { name: "Y", algorithm: "F R U' R' U' R U R' F' R U R' U' R' F R F'" }, // correct
+  { name: "H", algorithm: "M2 U M2 U2 M2 U M2" }, // correct
+  { name: "Ua", algorithm: "R U' R U R U R U' R' U' R2" }, // correct
+  { name: "Ub", algorithm: "R2 U R U R' U' R' U' R' U R'" }, // correct
+  { name: "Z", algorithm: "M' U M2 U M2 U M' U2 M2 U'" }, // added U' at the end for AUF. Might also need a Y to give us a more intuitive base view
 ];
